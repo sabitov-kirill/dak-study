e413c9a96ad4531b41c08e8adbe2f39993c7acb9
kondaevnil <defox4159@gmail.com>
2021-06-22 09:13:08 +0300

    file merged to pull new commit

--- client/src/pages/main/main.js
+++ client/src/pages/main/main.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
 import './main.css';
 
-<<<<<<< HEAD
-import LoginForm from '../../patterns/common/login-form/login-form'
-import PagesMenu from './body/scroll-menu'
-import InfoPanel from './body/info'
-=======
 import PagesMenu from './body/scroll-menu';
 import InfoPanel from './body/info';
 import Header from '../../patterns/common/header/header';
 import Footer from '../../patterns/common/footer/footer';
->>>>>>> 786259fb72bcf5700e80bef0265a039262168c1e
 
 class MainPage extends Component {
     render() {
